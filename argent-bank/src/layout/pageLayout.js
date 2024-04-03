1bd17d51b04bab4da815d8813a1cd9794cb428ae
christine ben <christinrbenabdallah@gmail.com>
2024-04-03 15:23:53 +0200

    correctif (ajout du code pour que la mise à jour du nom d'utilisateur ne sera pas effectuée si le nom d'utilisateur est vide ou contient uniquement des espaces.

--- argent-bank/src/layout/pageLayout.js
+++ argent-bank/src/layout/pageLayout.js
@@ -10,6 +10,3 @@ function PageLayout() {
   );
 }
 export default PageLayout;
-/* <NavBar /> En le plaçant d'abord, nous nous assurons qu'il est affiché en haut de chaque page. 
-Le Outlet est comme une zone réservée où le contenu de nos pages va être affiché.
- C'est là que le contenu spécifique à chaque page, défini par nos routes, apparaîtra.*/
